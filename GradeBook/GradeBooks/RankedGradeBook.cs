d347fd023e5acf359fde5d8671314b35b7bf74a1
s214008231 <s214008231@nmmu.ac.za>
2018-02-19 13:05:29 +0200

    PercentileLetterGrades

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,23 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             base.Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            var thresh = (int)Math.Ceiling(Students.Count * 0.2);
+            var average = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (averageGrade >= average[thresh - 1])
+                return 'A';
+            else if (averageGrade >= average[(thresh * 2) - 1])
+                return 'B';
+            else if (averageGrade >= average[(thresh * 3) - 1])
+                return 'C';
+            else if (averageGrade >= average[(thresh * 4) - 1])
+                return 'D';            
+            return 'F';
         }
     }
 }
